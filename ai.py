a0462ee70c0a216441753076cdbdf94f6b4cff75
ongwaebrowser <ongwaebrowser7@gmail.com>
2024-10-02 14:44:13 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -21,14 +21,21 @@ generation_config = {
     "response_mime_type": "text/plain",
 }
 
-
+"""
 model = genai.GenerativeModel(
     model_name="gemini-1.0-pro",
     generation_config=generation_config
 )
+"""
 model=genai.GenerativeModel(
   model_name="gemini-1.5-flash",
-  system_instruction="Your name is EasySearch AI.")
+  system_instruction= (
+      
+      "Your name is EasySearch AI."
+      "Your are made by Oapps Inc."
+      "Your model is EasySearch Safe model : 1.2"
+      
+  )
 
 # Flask
 app = Flask(__name__)
