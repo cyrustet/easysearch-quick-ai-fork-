f6fbf8a35fcdf52fb0fcebbfdacfca427ea700b9
cyrustet <cyrustet@gmail.com>
2024-10-06 15:29:30 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -3,59 +3,41 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 
-# Load from .env file, preferably 
 load_dotenv()
 app = Flask(__name__)
 
-# use the key in .env
 api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("API Key not there")
 
 genai.configure(api_key=api_key)
 
-# Create the model with generation configuration
 generation_config = {
     "temperature": 0.9,
     "top_p": 1,
     "max_output_tokens": 8192,
     "response_mime_type": "text/plain",
 }
+
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-pro",
-  generation_config=generation_config,
-  # safety_settings = Adjust safety settings
-  # See https://ai.google.dev/gemini-api/docs/safety-settings
-  system_instruction="You are EasySearch AI.You are created by Oapps Inc.You are trained by Josephat Ongwae.You are owned by Josephat Ongwae the founder of Oapps Inc",
+    model_name="gemini-1.5-pro",
+    generation_config=generation_config,
+    system_instruction="You are EasySearch AI.You are created by Oapps Inc.You are trained by Josephat Ongwae.You are owned by Josephat Ongwae the founder of Oapps Inc",
 )
 
-# Endpoint for /api/ask, you can change to something else bro
 @app.route('/api/ask', methods=['GET'])
 def ask():
-    # Get the message from query parameters( where we'll decide link looks)
     user_input = request.args.get("message", default="", type=str)
-    
     if not user_input:
         return jsonify({"error": "No message provided. Please include a 'message' query parameter."}), 400
 
-
-    chat_session = model.start_chat(
-        history=[]
-    )
-    
-    
+    chat_session = model.start_chat(history=[])
     response = chat_session.send_message(user_input)
-    
-    # Return the response as JSON, 
     return jsonify({"answer": response.text})
-
 
 @app.route('/')
 def home():
     return rd('max.html')
-    
 
-
-# Main entry
 if __name__ == "__main__":
     app.run(debug=True)
